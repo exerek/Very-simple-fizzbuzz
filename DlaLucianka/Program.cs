b1da12eb9552f8fd4c0bdd1e1ee15e4d766216ff
exerek <58123596+exerek@users.noreply.github.com>
2020-08-18 13:12:52 +0200

    Very simple and old fizzbuz

---
+++ DlaLucianka/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DlaLucianka
+{
+    public class Program
+    {
+        public static string GetOutPut(int number)
+        {
+            string output;
+
+            if ((number % 3 == 0) && (number % 5 == 0))
+            {
+                output = "Lucian";
+            }
+            else if (number % 3 == 0)
+            {
+                output = "Fellatio";
+            }
+            else if (number % 5 == 0)
+            {
+                output = "Ligawkurwia";
+            }
+            else
+            {
+                output = number.ToString();
+            }
+
+            return output;
+        }
+    }
+}
---
+++ DlaLucianka/lucianogotti.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DlaLucianka
+{
+    class lucianogotti
+    {
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(Program.GetOutPut(i));
+            }
+
+            Console.WriteLine("Zegnaj slonko mam nadzieje ze sie jeszcze zobaaczymy :)");
+            Console.ReadLine();
+        }
+    }
+}
